0ed25ab1f007c98dbba1ab93d7b662b384f192dc
G <griffin8418@gmail.com>
2022-09-01 23:03:18 +0800

    修正ICON已可正常顯示，簡化搜尋功能並完成
    -main.handlebars//修改title 從Document> My Restaurant List ;修改CSS連結及其載入順序;修改JS連結
    -index.handlebars//修改search 顯示出method=''GET'';刪去未完整程式碼; 將ICON顯示於指定位置內
    -app.js//簡化search功能，搜尋不分英文字母大小

--- app.js
+++ app.js
@@ -25,32 +25,17 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant })
 })
-//routes Search
+//routes function_Search
 app.get('/search', (req, res) => {
-  if (!req.query.keyword) {
-    return res.redirect('/')
-  }
+  console.log('req.keyword', req.query.keyword)
   const keyword = req.query.keyword
-  // const keyword = req.query.keywords.toLowerCase()
-
-  let restaurant = restaurant.filter(
-    restaurant =>
-      restaurant.name.toLowerCase().includes(keyword) || restaurant.category.includes(keyword)
-  )
-  res.render("index", { restaurant: restaurant })
-  // let conditionResult = ''
-  // const condition = req.query.condition
-  // const keyword = req.query.keyword
-
-  // if (condition === 'name') {
-  //   restaurants = restaurantList.results.filter(restaurant => {
-  //     return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-  //   })
-  // } else if (condition === 'category') {
-  //   restaurants = restaurantList.results.filter(restaurant => { return (restaurant.category.toLowerCase().includes) })
-  // }
-  // res.render('index', { restaurants: restaurantList.results })
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants })
 })
+
+
 //start and listen on the Express sever
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
